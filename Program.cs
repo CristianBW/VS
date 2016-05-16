34d95bec41a813d93840544378bfb79bacd14188
CristianBW <cristian.baciu@evozon.com>
2016-05-16 11:19:11 +0300

    adding from vs

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitPlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Run();          
+        }
+        void Run()
+        {
+            greetingConfirmation();
+        }
+        void greetingConfirmation()
+        {
+            Console.WriteLine("Hi ! This is GitPlayground, a project i created to test the functionaties of Git");
+            Console.WriteLine("Press any key to close the app.");
+            Console.Read();
+        }
+    }
+}
